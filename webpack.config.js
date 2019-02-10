4798c384714339f0744740cae2e189a04c3d01d5
Andy Chen <amazingandyyy@gmail.com>
2019-02-09 22:22:42 -0800

    passing store down and use state-api to passing down actions

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,16 @@ const config = {
         test: /\.js$/, 
         exclude: /node_modules/, 
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties'
+            ]
+          }
         }
       }
     ]
